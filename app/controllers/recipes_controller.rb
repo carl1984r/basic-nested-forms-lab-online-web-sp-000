c3ed03228c2977a03a29dcb2f1afca19536e7381
Bry Cox <carl.cc@gmail.com>
2019-08-15 07:09:33 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,7 +29,8 @@ class RecipesController < ApplicationController
 
   private
 
-  def recipe_params
+  def recipe_params 
+    binding.pry
     params.require(:recipe).permit(:title, ingredients_attributes: [:name, :quantity])
   end
 
